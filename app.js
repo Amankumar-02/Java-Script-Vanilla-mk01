5bbe48511247b7ede71a22fa10bcf155ed9ba072
Aman <ak193566@gmail.com>
2023-04-12 00:42:02 +0530

    docs : update commit

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ var outputDiv = document.querySelector("#output");
 // console.log(translatebtn);
 // console.log(txtarea);
 // console.log(outputDiv);
-var serverURL =
-  "https://translation-api.amankumar511.repl.co/translate/yoda.json";
-function getTranslationURL(text) {
-  return serverURL + "?" + "text=" + text;
+// var serverURL =
+//   "https://translation-api.amankumar511.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input;
 }
 function errorHandler(error) {
   console.log("error occured", error);
-  alert("Something Wrong Happen with the server!! Try after some time.")
+  alert("Something Wrong Happen with the server!! Try after some time.");
 }
 function clickHandler() {
   // console.log("clicked!");
@@ -20,7 +21,11 @@ function clickHandler() {
   var inputText = txtarea.value;
   fetch(getTranslationURL(inputText))
     .then((response) => response.json())
-    .then((json) => console.log(json.contents.translated))
+    .then((json) => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        //  json.console.log(json.contents.translated);
+    })
     .catch(errorHandler);
 }
 translatebtn.addEventListener("click", clickHandler);
